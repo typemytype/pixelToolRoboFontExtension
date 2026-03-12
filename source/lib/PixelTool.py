ada72effdd4cb3f7a28783c35b8b4747c326d489
Ryan Bugden <rbugden@gmail.com>
2026-03-12 00:47:16 -0400

    Hook UI up, support non-standard grid
    Fix https://github.com/typemytype/pixelToolRoboFontExtension/issues/12

--- source/lib/PixelTool.py
+++ source/lib/PixelTool.py
@@ -10,17 +10,29 @@ from fontTools.pens.pointPen import ReverseContourPointPen
 from mojo.events import BaseEventTool, installTool
 from mojo.roboFont import CreateCursor, version
 from mojo.extensions import getExtensionDefault, setExtensionDefault, ExtensionBundle
-from mojo.UI import getDefault, setDefault
-from lib.tools.notifications import PostNotification
+from mojo.UI import getDefault, setDefault, preferencesChanged
 
-from settings import *
 from generateImages import AddPixelToolRepresentationFactory
+
 
 AddPixelToolRepresentationFactory()
 
 pixelBundle = ExtensionBundle("Pixel Tool")
 pixelCursor = CreateCursor(pixelBundle.get("pixelCursor"), hotSpot=(1, 19))
 pixelToolbarIcon = pixelBundle.get("pixelToolbarIcon")
+
+ADD_ACTION_MODE = "Adding"
+REMOVE_ACTION_MODE = "Removing"
+DRAWING_MODES = ["Rectangle", "Oval", "Component"]
+EXT_KEY = "com.typemytype.pixelTool"
+DEFAULTS = {
+    "pixelWidth": 50,
+    "pixelHeight": 50,
+    "useGridPosition": True,
+    "useGridSize": True,
+    "pixelShape": 0,
+    "baseGlyph": "pixel",
+}
 
 
 def _roundPoint(x, y):
@@ -29,7 +41,8 @@ def _roundPoint(x, y):
 
 class GridSettingsPopoverController(ezui.WindowController):
 
-    def build(self, parent, location):
+    def build(self, tool, parent, location):
+        self.editingTool = tool
         self.x, self.y = location
         content = """
         !§ Pixel Tool Settings
@@ -81,9 +94,10 @@ class GridSettingsPopoverController(ezui.WindowController):
             controller=self,
             size="auto"
         )
-        self.formCallback(self.w.getItem("form"))
 
     def started(self):
+        self.w.setItemValues(getExtensionDefault(EXT_KEY, fallback=DEFAULTS))
+        self.formCallback(self.w.getItem("form"))
         self.w.open(location=(self.x - 5, self.y - 5, 10, 10))
 
     def formCallback(self, sender):
@@ -94,97 +108,27 @@ class GridSettingsPopoverController(ezui.WindowController):
             self.w.getItem(identifier).enable(not False in states)
         # Gray out the base glyph text field if "Component" isn’t selected.
         self.w.getItem("baseGlyph").enable(self.w.getItem("pixelShape").get() == 2)
-
-
-class GridSettingsMenu(object):
-
-    '''Deprecated'''
-
-    def __init__(self, tool, event, view):
-        self.tool = tool
-
-        self.drawingChoices = [RECT_MODE, OVAL_MODE, COMPONENT_MODE]
-
-        self.view = vanilla.Group((0, 0, 0, 0))
-        nsView = self.view.getNSView()
-        nsView.setFrame_(AppKit.NSMakeRect(0, 0, 185, 165))
-
-        self.view.gridText = vanilla.TextBox((10, 12, 100, 22), "Pixel Size:")
-        self.view.gridInput = vanilla.EditText((120, 10, -10, 22), self.tool.size, callback=self.gridInputCallback)
-
-        self.view.drawingMode = vanilla.RadioGroup((10, 40, -10, 75), self.drawingChoices, isVertical=True, callback=self.drawingModeCallback)
-        if self.tool.drawingMode in self.drawingChoices:
-            self.view.drawingMode.set(self.drawingChoices.index(self.tool.drawingMode))
-
-        self.view.componentName = vanilla.EditText((120, 90, -10, 22), self.tool.componentName, callback=self.drawingModeCallback)
-        self.view.componentName.show(self.tool.drawingMode == COMPONENT_MODE)
-
-        self.view.useGrid = vanilla.CheckBox((11, 125, -10, 22), "Use Grid", value=self.tool.useGrid, callback=self.drawingModeCallback)
-
-        menu = AppKit.NSMenu.alloc().init()
-        settingsItem = AppKit.NSMenuItem.alloc().initWithTitle_action_keyEquivalent_("doodle.guideView", None, "")
-        settingsItem.setView_(nsView)
-        menu.addItem_(settingsItem)
-
-        AppKit.NSMenu.popUpContextMenu_withEvent_forView_(menu, event, view)
-
-    def gridInputCallback(self, sender):
-        value = sender.get()
-        # must be int
-        try:
-            value = int(value)
-        except Exception:
-            value = -1
-
-        if value <= 0:
-            value = self.tool.size
-            sender.set(value)
-            return
-
-        self.tool.size = value
-        setExtensionDefault(GRID_DEFAULTS_KEY, value)
-
-    def drawingModeCallback(self, sender):
-        i = self.view.drawingMode.get()
-
-        value = self.drawingChoices[i]
-
-        self.tool.drawingMode = value
-        setExtensionDefault(DRAWING_DEFAULTS_KEY, value)
-        componentName = ""
-        if value == COMPONENT_MODE:
-            self.view.componentName.show(True)
-            componentName = str(self.view.componentName.get())
-        else:
-            self.view.componentName.show(False)
-            self.view.componentName.set("")
-        self.tool.componentName = componentName
-        setExtensionDefault(COMPONENT_DEFAULT_KEY, componentName)
-
-        useGrid = self.view.useGrid.get()
-        self.tool.useGrid = useGrid
-        setExtensionDefault(USEGRID_DEFAULT_KEY, useGrid)
+        # Set the text fields to grid size is desired.
+        if self.w.getItem("useGridSize").get():
+            self.w.getItem("pixelWidth").set(getDefault("glyphViewGridx"))
+            self.w.getItem("pixelHeight").set(getDefault("glyphViewGridy"))
+        setExtensionDefault(EXT_KEY, self.w.getItemValues())
+        self.editingTool.setup()
 
 
 class PixelTool(BaseEventTool):
 
-    def _get_size(self):
-        return self._size
-
-    def _set_size(self, value):
-        self._size = value
-        setDefault("glyphViewGridx", value)
-        setDefault("glyphViewGridy", value)
-        PostNotification("doodle.preferencesChanged")
-
-    size = property(_get_size, _set_size)
-
     def setup(self):
-        self.size = int(getExtensionDefault(GRID_DEFAULTS_KEY, 50))
+        if getExtensionDefault(EXT_KEY) is None:
+            print("DEFAULT IS NONE!! REsetting to defaults")
+            setExtensionDefault(EXT_KEY, DEFAULTS)
+        self.width = int(getExtensionDefault(EXT_KEY)["pixelWidth"])
+        self.height = int(getExtensionDefault(EXT_KEY)["pixelHeight"])
         self.actionMode = ADD_ACTION_MODE
-        self.drawingMode = getExtensionDefault(DRAWING_DEFAULTS_KEY, RECT_MODE)
-        self.componentName = getExtensionDefault(COMPONENT_DEFAULT_KEY, "")
-        self.useGrid = getExtensionDefault(USEGRID_DEFAULT_KEY, True)
+        self.drawingMode = DRAWING_MODES[getExtensionDefault(EXT_KEY)["pixelShape"]]
+        self.componentName = getExtensionDefault(EXT_KEY)["baseGlyph"]
+        self.useGridPos = getExtensionDefault(EXT_KEY)["useGridPosition"]
+        self.useGridSize = getExtensionDefault(EXT_KEY)["useGridSize"]
 
     def mouseDown(self, point, offset):
         glyph = self.getGlyph()
@@ -195,7 +139,7 @@ class PixelTool(BaseEventTool):
         if found is not None:
             # remove contour if we found one
             self.actionMode = REMOVE_ACTION_MODE
-            if self.drawingMode == COMPONENT_MODE:
+            if self.drawingMode == "Component":
                 glyph.removeComponent(found)
             else:
                 glyph.removeContour(found)
@@ -205,18 +149,17 @@ class PixelTool(BaseEventTool):
             self.addShapeInGlyphForPoint(glyph, point)
 
     def _rightMouseDown(self, point, event):
-        # view = self.getNSView()
-        # point = view.window().mouseLocationOutsideOfEventStream()
-        # x, y = view.convertPoint_fromView_(point, None)
-        # GridSettingsPopoverController(view, (x, y))
-        GridSettingsMenu(self, event, self.getNSView())
+        view = self.getNSView()
+        point = view.window().mouseLocationOutsideOfEventStream()
+        x, y = view.convertPoint_fromView_(point, None)
+        GridSettingsPopoverController(self, view, (x, y))
 
     def mouseDragged(self, point, delta):
         glyph = self.getGlyph()
         found = self.findObjectInGlyphForPoint(glyph, point)
 
         if self.actionMode == REMOVE_ACTION_MODE and found is not None:
-            if self.drawingMode == COMPONENT_MODE:
+            if self.drawingMode == "Component":
                 glyph.removeComponent(found)
             else:
                 glyph.removeContour(found)
@@ -233,7 +176,7 @@ class PixelTool(BaseEventTool):
     def findObjectInGlyphForPoint(self, glyph, point):
         x, y = point.x, point.y
         found = None
-        if self.drawingMode == COMPONENT_MODE:
+        if self.drawingMode == "Component":
             for component in glyph.components:
                 if component.baseGlyph != self.componentName:
                     continue
@@ -248,9 +191,9 @@ class PixelTool(BaseEventTool):
         return found
 
     def addShapeInGlyphForPoint(self, glyph, point):
-        w = h = self.size
+        w, h = self.width, self.height
 
-        if self.useGrid:
+        if self.useGridPos:
             x = int(floor(point.x / float(w))) * w
             y = int(floor(point.y / float(h))) * h
         else:
@@ -261,7 +204,7 @@ class PixelTool(BaseEventTool):
         if glyph.preferredSegmentType == "qcurve":
             pen = ReverseContourPointPen(pen)
 
-        if self.drawingMode == RECT_MODE:
+        if self.drawingMode == "Rectangle":
             pen.beginPath()
             pen.addPoint(_roundPoint(x, y), "line")
             pen.addPoint(_roundPoint(x + w, y), "line")
@@ -270,7 +213,7 @@ class PixelTool(BaseEventTool):
 
             pen.endPath()
 
-        elif self.drawingMode == OVAL_MODE:
+        elif self.drawingMode == "Oval":
 
             hw = w / 2.
             hh = h / 2.
@@ -299,7 +242,7 @@ class PixelTool(BaseEventTool):
 
             pen.endPath()
 
-        elif self.drawingMode == COMPONENT_MODE and self.componentName and self.componentName != glyph.name:
+        elif self.drawingMode == "Component" and self.componentName and self.componentName != glyph.name:
             pen.addComponent(self.componentName, [1, 0, 0, 1, x, y])
 
     def getDefaultCursor(self):
